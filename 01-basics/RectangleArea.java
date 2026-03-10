9149708c477cc06e2635c5f4a4601838287f1ba0
Prathima S <86216754+Prathima16@users.noreply.github.com>
2026-03-10 20:59:28 +0530

    Added  RectangleArea program

---
+++ 01-basics/RectangleArea.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	        int length = 20;
+	        int width = 5;
+
+	        int area = length * width;
+
+	        System.out.println("Area of Rectangle: " + area);
+	}
+
+}
