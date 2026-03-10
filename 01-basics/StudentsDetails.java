ed4b2979a5f6f883f005ad208e512856d7bc0df6
Prathima S <86216754+Prathima16@users.noreply.github.com>
2026-03-10 20:55:30 +0530

    Added  StudentsDetails program

---
+++ 01-basics/StudentsDetails.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class StudentDetails {
+
+	    public static void main(String[] args) {
+
+	        String name = "Rakesh";
+	        int age = 21;
+	        double marks = 85.5;
+
+	        System.out.println("Name: " + name);
+	        System.out.println("Age: " + age);
+	        System.out.println("Marks: " + marks);
+	    }
+	}
+
