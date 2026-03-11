2247a8ecb3d75b7f471d35a08d9114cd0c44d698
Prathima S <86216754+Prathima16@users.noreply.github.com>
2026-03-11 13:04:24 +0530

    Added WrapperClass program

---
+++ 01-basics/WrapperClass.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Auto-boxing
+		
+		int num=70;//primitive
+		
+		Integer obj=num;//auto-boxing -obj:wrapper object
+		
+	 System.out.println("Primitive value: "+ num);
+	 System.out.println("Wrapper object: "+ obj);
+	}
+
+}
