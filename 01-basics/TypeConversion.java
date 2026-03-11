90712aa0afb4b9848954fc21bada7a299f75d71c
Prathima S <86216754+Prathima16@users.noreply.github.com>
2026-03-11 14:54:34 +0530

    Added TypeConversion program

---
+++ 01-basics/TypeConversion.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double d= sc.nextDouble();
+		int result =(int)d;
+		System.out.println(result);
+		
+		sc.close();
+	}
+
+}
+
+output:
+8000000
+8000000
+
+
+  34444777777
+2147483647
