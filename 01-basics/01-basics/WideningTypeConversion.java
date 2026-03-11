b65ef5b36cba1c22c0f286a05e6651141ab509e9
Prathima S <86216754+Prathima16@users.noreply.github.com>
2026-03-11 13:57:39 +0530

    Added WideningTypeConversion program

---
+++ 01-basics/01-basics/WideningTypeConversion.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class WideningTypeConversion {
+
+	    public static void main(String[] args) {
+
+	        int num = 10;
+	        double value = num;   // int → double
+
+	        System.out.println(num);
+	        System.out.println(value);
+
+	    }
+	}
+
