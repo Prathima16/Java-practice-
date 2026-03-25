3ab8ffa58d739d4afe88b26c7e6ea6d4e3805dac
Prathima S <86216754+Prathima16@users.noreply.github.com>
2026-03-25 14:18:56 +0530

    Add WhileAndDoWhile Examples

---
+++ loops/WhileAndDoWhile.java
@@ -0,0 +1,34 @@
+public class WhileAndDoWhileLoop {
+    public static void main(String[] args) {
+1.  //while
+        /*syntax
+        while(condition){
+            //body
+        }*/
+
+       //print 1-5
+        int num=1;
+        while(num<=5){
+            System.out.println(num);
+            num+=1;
+    }
+
+
+2.    //Do-while
+        /*syntax
+        do{
+        //body
+        }while(condition)
+         */
+
+    //print 1-5 no's
+    int n = 1;
+        do
+
+    {
+        System.out.println(n);
+        n++;
+    }while(n<=5);
+}
+}
+
